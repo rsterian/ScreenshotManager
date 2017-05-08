2b7cf1d7f2d64a6a97508c27e4f85ba8025f5c6c
rsterian <rsterian@umich.edu>
2017-05-08 16:31:06 -0700

    Initial Commit

---
+++ Screenshot Manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Screenshot Manager
+//
+//  Created by Riley Sterian on 5/8/17.
+//  Copyright © 2017 rileyq. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Screenshot Manager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Screenshot Manager
+//
+//  Created by Riley Sterian on 5/8/17.
+//  Copyright © 2017 rileyq. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
