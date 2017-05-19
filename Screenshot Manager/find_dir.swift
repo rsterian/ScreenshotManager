5328606460022159999a50d70fb5364211a95571
rsterian <rsterian@umich.edu>
2017-05-19 14:53:34 -0700

    Cleaned up comments

--- Screenshot Manager/find_dir.swift
+++ Screenshot Manager/find_dir.swift
@@ -10,11 +10,6 @@ import Cocoa
 import Foundation
 import EonilFileSystemEvents
 
-//func compHandler(input: Int) -> Void{
-//    //print(String(input) + " in the comphandler")
-//    return
-//}
-
 //https://gist.github.com/ericdke/fec20e6db9e0aa25e8ea
 func show_notification(ss_name:String, dest:String) -> Void {
     let notification = NSUserNotification()
@@ -30,10 +25,7 @@ func get_dir() -> String{
     panel.canChooseDirectories = true
     panel.canChooseFiles = false
     panel.canCreateDirectories = true
-    
-    //one way to get the pathname in the format:
-    //  /Users/rsterian/Desktop/
-    
+
     var url_string:String
     
     let user_response = panel.runModal()
@@ -44,64 +36,9 @@ func get_dir() -> String{
     } else {
         url_string = "ERROR"
     }
-    //print(url_string)
     
     return url_string
 }
-
-//func make_temp(path: String){
-//    
-//    print("in make_temp")
-//    
-//    let fm = FileManager()
-//    let full_path:String = path + "/Screenshot Manager Parent/"
-//    if(fm.fileExists(atPath: full_path)){
-//        //temp already exists
-//        print("path already exists, but that's okay")
-//        return
-//    }
-//    print("trying to make a directory at ")
-//    print(full_path)
-//    
-//    do{
-//        try fm.createDirectory(atPath: full_path, withIntermediateDirectories: false, attributes: nil)
-//        print("Success! Directory created")
-//    } catch let error as NSError{
-//        //print("Error: Invalid pathname")
-//        print(error.description)
-//        print(error.localizedFailureReason!)
-//    }
-//    
-//}
-
-//TODO
-//doesn't actually check anything yet
-//func check_for_updates(path: String){
-//    print("in check_for_updates")
-//    if(path == ""){
-//        return
-//    }
-//    print(path)
-//    let fm = FileManager()
-//    do{
-//        try print(fm.contentsOfDirectory(atPath: path))
-//    } catch let error as NSError{
-//        print("caught")
-//        //print(error.description)
-//        print(error.localizedFailureReason!)
-//    }
-//    
-//}
-
-//func print_running_app(){
-//    let workspace = NSWorkspace()
-//    for element in workspace.runningApplications{
-//        print(element.localizedName!)
-//    }
-//    sleep(5)
-//    print("FRONTMOST APP IS ")
-//    print(workspace.frontmostApplication!.localizedName!)
-//}
 
 //http://stackoverflow.com/questions/39513258/get-current-date-in-swift-3
 //http://www.unicode.org/reports/tr35/tr35-25.html#Date_Format_Patterns
@@ -110,14 +47,12 @@ func get_format_time() -> String{
     let day_formatter = DateFormatter()
     day_formatter.dateFormat = "yyyy-MM-dd 'at' h.mm.ss a"
     let result = day_formatter.string(from: date)
-    //print(result)
     return result
 }
 
 //https://code.tutsplus.com/tutorials/swift-and-regular-expressions-swift--cms-26626
 func path_is_screenshot(path: String) -> Bool{
     let path_components = path.components(separatedBy: "/")
-    //print(path_components)
     let ss_name = path_components[path_components.count - 1]
     
     // (1): 
@@ -135,41 +70,19 @@ func path_is_screenshot(path: String) -> Bool{
 func get_time_from_path(path: String) -> String{
     assert(path_is_screenshot(path: path))
     let path_components = path.components(separatedBy: "/")
-    //print(path_components)
     let ss_name = path_components[path_components.count - 1]
 
-    //print("in get_time_from_path")
     var time = ss_name
     time.removeSubrange(time.startIndex..<time.index(time.startIndex, offsetBy: 12)) //remove up to time
     
     time.removeSubrange(time.index(time.endIndex, offsetBy: -4)..<time.endIndex) //remove .png
-    //print(time)
     return time
-    
-    
-    
-//    let text = "123045780984"
-//    if let rangeOfZero = text.range(of: "0", options: .backwards) {
-//        // Found a zero
-//        let suffix = String(text.characters.suffix(from: rangeOfZero.upperBound)) // "984"
-//    }
 }
 
 func match_handler(source: String, dest: String, file: String, time:String, notify:Bool, frontmost:String){
-    
-    ////////
-    //get_time_from_path(path: file)
-    ////////
-    
-//    let workspace = NSWorkspace()
-//    let frontmost:String = workspace.frontmostApplication!.localizedName!
-    
     let fm = FileManager()
     let new_path:String = dest + frontmost
-    
-    //print("Going to move " + file + " from " + source + " to " + new_path)
 
-    
     if(!fm.fileExists(atPath: new_path)){
         do{
             try fm.createDirectory(atPath: new_path, withIntermediateDirectories: false, attributes: nil)
@@ -179,22 +92,17 @@ func match_handler(source: String, dest: String, file: String, time:String, noti
     }
     
     if(fm.fileExists(atPath: new_path + "/Screenshot " + time + ".png")){
-        //print("didn't actually move this time")
         return
     }
     
     do{
-        //change the toPath argument to be fine with .png 1 or some other int
-        //maybe new function to just get  from Screen Shot on...?
         try fm.moveItem(atPath: file, toPath: new_path + "/Screenshot " + time + ".png")
         if(notify){
             show_notification(ss_name: file, dest: new_path)
         }
         return 
-    } catch let _ as NSError{
+    } catch _ as NSError{
         //print(error.localizedFailureReason!)
-//        print(file)
-//        print(new_path + "/Screen Shot " + get_format_time())
         //print(error.localizedDescription)
     }
     return
